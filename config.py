81b938d8346839d4aaac105b34da37260a47abbc
eric chen <eric.chen.uoa@gmail.com>
2020-07-03 15:54:09 +1200

    initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+PATH_TRAIN = "foetal-head-us/train/"
+PATH_TEST = "foetal-head-us/test/"
+
+
+# set target image size
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
+
+epochs = 50
+batch_size = 16
+
+#division smooth
+smooth = 1.
